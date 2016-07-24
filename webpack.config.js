f79f1e2788ce4d766cf22898aa893734352da610
Filip Dupanović <filip.dupanovic@gmail.com>
2016-07-24 10:37:55 +0200

    Add base Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval',
+  target: 'web',
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: 'dist',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot-loader', 'babel'],
+      include: 'src'
+    }, {
+      test: /\.scss$/,
+      loaders: 'sass',
+      loader: 'style!css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox'
+    }, {
+      test: /\.css$/,
+      loaders: 'style!css'
+    }]
+  }
+};
