d04eb63a81bebac245469917bba60d1222535310
Filip Dupanović <filip.dupanovic@gmail.com>
2016-07-25 17:06:58 +0200

    Hello World

---
+++ src/components.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// Main component.
+export const Praline = () => (
+  <h1>Hello World</h1>
+);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import 'normalize.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {Praline} from './components';
+
+/**
+ * IIFE. Mounts the main component to the page.
+ */
+(function main() {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+
+  ReactDOM.render(<Praline />, container);
+})();
