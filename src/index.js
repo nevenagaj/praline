0d15bbc962b27a026a74f7213b42f4911b89939c
Filip Dupanović <filip.dupanovic@gmail.com>
2016-07-29 18:39:16 +0200

    Add fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import 'normalize.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import WebFont from 'webfontloader'
+
+// Load fonts.
+WebFont.load({
+  classes: false,
+  events: false,
+  google: {
+    families: ['Amatic SC', 'Raleway:400,400i,500,700'],
+    text: 'abcdefghijklmnopqrstuvwxyz'
+  }
+})
 
 import {Praline} from './components'
 
