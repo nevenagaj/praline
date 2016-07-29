b86d5189be2cd2318f2c8336904039cdf7f1e648
Filip Dupanović <filip.dupanovic@gmail.com>
2016-07-29 21:23:58 +0200

    Extract font loading

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import 'normalize.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import WebFont from 'webfontloader'
-
-// Load fonts.
-WebFont.load({
-  classes: false,
-  events: false,
-  google: {
-    families: ['Amatic SC', 'Raleway:400,400i,500,700'],
-    text: 'abcdefghijklmnopqrstuvwxyz'
-  }
-})
 
 import {Praline} from './components'
+import webfont from './lib/webfont'
 
 // IIFE. Mounts the main component to the page.
 (function main () {
+  // Load fonts.
+  webfont({
+    families: ['Amatic SC'],
+    text: 'Pralines'
+  })
+
+  webfont({
+    families: ['Raleway:400,400i,500,700'],
+    text: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  })
+
   const container = document.createElement('article')
   document.body.appendChild(container)
 
