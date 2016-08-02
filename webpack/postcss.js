b0847cbfc9c62989ded6b366135fcc8012e61f86
Filip Dupanović <filip.dupanovic@gmail.com>
2016-08-02 07:08:52 +0200

    Configure stylelint

--- webpack/postcss.js
+++ webpack/postcss.js
@@ -1,11 +1,21 @@
 const cssnext = require('postcss-cssnext')
 const importer = require('postcss-import')
 const reporter = require('postcss-reporter')
+const stylelint = require('stylelint')
+
+const stylelintConfiguration = {
+  extends: 'stylelint-config-standard',
+  rules: []
+}
 
 // A callable that provides the PostCSS configuration.
 module.exports = (webpack) => ([
+  stylelint(stylelintConfiguration),
   importer({
-    addToDependency: webpack
+    addToDependency: webpack,
+    plugins: [
+      stylelint(stylelintConfiguration)
+    ]
   }),
   cssnext(),
   reporter()
